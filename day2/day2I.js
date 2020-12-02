71c1b894f5c639a1b9b1492a576e628912f6bc43
MaikaQuizzel <70568319+MaikaQuizzel@users.noreply.github.com>
2020-12-02 10:11:11 +0100

    in theory it should work but it dosn't

--- day2/day2I.js
+++ day2/day2I.js
@@ -1,9 +1,10 @@
-const { count } = require('console');
 const fs = require('fs');
 
 const array = fs.readFileSync('D:\\Eigene-Dateien\\Dokumente\\codeventure\\day2\\input2.txt','utf8');
 console.log(typeof array);
-const stingOfArray= array.replace('\r\n', ',');
+const stingOfArray= array.replace(/\r?\n|\r/, ',');
+console.log(stingOfArray);
+const array2 = stingOfArray.split(',');
 
 const min = 0;
 const max = 0;
@@ -11,11 +12,11 @@ const charecter = '';
 const password ='';
 const corectPW= 0;
 
-for(const entry of stingOfArray){ 
+for(const entry of array2){ 
     console.log(typeof entry);
     console.log(entry);
-    const spiltEntry = entry.split(' ',2); c
-    onsole.log(spiltEntry);
+    const spiltEntry = entry.split(' ',2); 
+    console.log(spiltEntry);
         const numbers = spiltEntry.split('-')[0];
         console.log(numbers);
             min = numbers[0];
@@ -28,4 +29,5 @@ for(const entry of stingOfArray){
         corectPW++;
     }
 }
+console.log(corectPW);
 
