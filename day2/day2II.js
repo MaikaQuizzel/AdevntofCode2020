aab87a03ab773f0728457d1494b285b08cccc3ce
MaikaQuizzel <70568319+MaikaQuizzel@users.noreply.github.com>
2020-12-07 14:57:14 +0100

    day 2 II parts

--- day2/day2II.js
+++ day2/day2II.js
@@ -6,12 +6,12 @@ const regEx1 = new RegExp(replaceType, 'g');
 var re = ':';
 var regEx2 = new RegExp(re, 'g');
 
-const array = fs.readFileSync('D:\\Eigene-Dateien\\Dokumente\\GitHub\\AdevntofCode2020\\day2\\input.txt', 'utf8');
+const array = fs.readFileSync('D:\\Eigene-Dateien\\Dokumente\\GitHub\\AdevntofCode2020\\day2\\inputTest.txt', 'utf8');
 const stingOfArray = array.replace(regEx1, ',');
 const array2 = stingOfArray.split(',');
 
-let min = 0;
-let max = 0;
+let firstPlace = 0;
+let secondPlace = 0;
 let charecter = '';
 let password = '';
 let corectPW = 0;
@@ -19,8 +19,8 @@ let corectPW = 0;
 for (const entry of array2) {
     const spiltEntry = entry.split(' ', 3);
     const numbers = spiltEntry[0].split('-', 2);
-    min = numbers[0];
-    max = numbers[1];
+    firstPlace = numbers[0];
+    secondPlace = numbers[1];
 
     charecter = spiltEntry[1];
     charecter = charecter.replace(':', '');
@@ -29,12 +29,20 @@ for (const entry of array2) {
     var regex = new RegExp(charecter, 'g');
 
     let charsInSplit = password.match(regex);
-    console.log(entry, min, max, charecter, password, _.size(charsInSplit));
+    console.log(entry, charecter); //, firstPlace, secondPlace, , password, _.size(charsInSplit)
 
     let numberOfChar = _.size(charsInSplit);
+    console.log(charecter === password[firstPlace] && charecter === password[secondPlace]);
+    console.log(charecter === password[firstPlace] || charecter === password[secondPlace]);
 
-    if (numberOfChar >= min && numberOfChar <= max) {
-        corectPW++;
+    if (charecter === password[firstPlace] && charecter === password[secondPlace]) {
+        console.log('1 und 1 gleich');
+    } else {
+        if (charecter === password[firstPlace] || charecter === password[secondPlace]) {
+            corectPW++;
+        } else {
+            console.log('warum bin ich hier?');
+        }
     }
     console.log(corectPW);
 }
