ea6fedaabd9fa388c71df8db585b14b9f006655c
MaikaQuizzel <70568319+MaikaQuizzel@users.noreply.github.com>
2020-12-15 08:03:11 +0100

    stuff

---
+++ 15/day15I.js
@@ -0,0 +1,15 @@
+const intup = [9, 19, 1, 6, 0, 5, 4];
+const numbersInArray = 7;
+let arrayOfNumbers = intup;
+
+while (numbersInArray >= 2020) {
+    //if last number ==firsttimespoken -> newNumber ==0;if( firsttimespoken(0))turnsappart
+}
+function firstTimeSPoken(number, arrayOfNumbers) {
+    const countOccurrences = (arrayOfNumbers, number) => arr.reduce((a, v) => (v === val ? a + 1 : a), 0);
+    return Boolean;
+}
+
+function TurnsAppart(number) {
+    return Number;
+}
